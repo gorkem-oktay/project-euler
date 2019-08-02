b6d171d691fd70626bc360a9573a70cb2373670e
Gorkem Oktay <gorkem.oktay@masomo.com>
2019-08-02 14:46:08 +0300

    solved 13

--- src/com/zafiru/Main.java
+++ src/com/zafiru/Main.java
@@ -21,6 +21,6 @@ public class Main {
             problems.put(i, problem.getDeclaredConstructor().newInstance());
         }
 
-        System.out.println(problems.get(12).answer());
+        System.out.println(problems.get(13).answer());
     }
 }
